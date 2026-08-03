98e38e48667cc3eed2549f59528614d0a7de9752
buffeddude69 <tolaybagz@gmail.com>
2026-08-03 23:57:39 +0800

    change again

--- church_tracker/config/settings.py
+++ church_tracker/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1', '.render.com').split(',')
 
 
 # Application definition
