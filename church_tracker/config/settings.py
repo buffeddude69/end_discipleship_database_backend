d1b40e90c05f7b9f696690d4810eb2850a67d0db
buffeddude69 <tolaybagz@gmail.com>
2026-08-28 12:18:00 +0800

    SLIGHT CHANGES
    - staff is only allowed to register leader accounts
    - added campus area & demography input
    - member citizenship

--- church_tracker/config/settings.py
+++ church_tracker/config/settings.py
@@ -139,7 +139,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ.get('DB_NAME', 'church_tracker'),
             'USER': os.environ.get('DB_USER', 'postgres'),
-            'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres123'),
+            'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
             'HOST': os.environ.get('DB_HOST', 'localhost'),
             'PORT': os.environ.get('DB_PORT', '5432'),
         }
